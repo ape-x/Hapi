eee6c935d4431b7d09b76f84415e40eb796f71d1
Mihnea Stefan <mihnea.stefan@protonmail.com>
2021-11-12 20:27:38 +0200

    Initial Commit

---
+++ Hapi/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Hapi
+//
+//  Created by Mihnea Stefan on 12.11.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
